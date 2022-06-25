d3b2f9ef46763a57ee89d7378ce170d0e9123248
Vineet Kumar <jammackp@gmail.com>
2022-06-25 22:51:42 +0530

    search bar removed

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -2,21 +2,19 @@ import React from "react";
 import styles from "./style.module.css";
 import logo from "assets/logo/logo.png";
 import { Search } from "assets/icons";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { useAuth } from "context";
 import { useAuthHandler } from "hooks";
 
 export function Header() {
   const { userAuthState } = useAuth();
   const { handleLogout } = useAuthHandler();
+
   return (
     <header className={styles.header}>
       <img src={logo} alt="Gimesto logo" />
       <div className={styles.rightCont}>
-        <div className={styles.searchCont}>
-          <input className={styles.searchInput} placeholder="Search" />
-          <Search />
-        </div>
+      
         {userAuthState.isLoggedIn ? (
           <Link to="/">
             <button className="btn btn-primary" onClick={handleLogout}>
